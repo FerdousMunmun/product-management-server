74ab652b13fb901eee74939ded9338d98ac93011
Jannatul Ferdous <ferdousmunmun75@gmail.com>
2026-08-12 06:32:02 +0600

    prepare backend for vercel

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,15 @@
 import express from "express";
 import cors from "cors";
-
 import routes from "./routes";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Product Management API Running");
+});
 
 app.use("/api/v1", routes);
 
