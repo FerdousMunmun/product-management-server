4763cc695b73d407e7bb72afc2671bd6b744d44d
Jannatul Ferdous <ferdousmunmun75@gmail.com>
2026-08-12 06:29:18 +0600

    add vercel config

--- src/services/categories/categories.ts
+++ src/services/categories/categories.ts
@@ -1,5 +1,79 @@
 import { Router } from "express";
+import prisma from "../../lib/prisma";
 
 const router = Router();
+
+
+router.get("/", async (req, res) => {
+  const categories = await prisma.category.findMany({
+    where: {
+         
+      isDeleted: false,
+    },
+  });
+
+  res.json({
+    success: true,
+    data: categories,
+  });
+});
+
+router.get("/:id", async (req, res) => {
+  const category = await prisma.category.findUnique({
+    where: {
+      id: req.params.id,
+      isDeleted: false,
+    },
+  });
+
+  res.json({
+    success: true,
+    data: category,
+  });
+});
+
+
+router.post("/", async (req, res) => {
+  const category = await prisma.category.create({
+    data: req.body,
+  });
+
+  res.json({
+    success: true,
+    data: category,
+  });
+});
+
+
+router.patch("/:id", async (req, res) => {
+  const category = await prisma.category.update({
+    where: {
+      id: req.params.id,
+    },
+    data: req.body,
+  });
+
+  res.json({
+    success: true,
+    data: category,
+  });
+});
+
+
+router.delete("/:id", async (req, res) => {
+  const category = await prisma.category.update({
+    where: {
+      id: req.params.id,
+    },
+    data: {
+      isDeleted: true,
+    },
+  });
+
+  res.json({
+    success: true,
+    data: category,
+  });
+});
 
 export default router;
--- src/services/users/users.ts
+++ src/services/users/users.ts
@@ -4,7 +4,19 @@ import prisma from "../../lib/prisma";
 const router = Router();
 
 router.get("/", async (req, res) => {
-  const users = await prisma.user.findMany();
+  const users = await prisma.user.findMany({
+
+    select: {
+    id: true,
+    name: true,
+    email: true,
+    role: true,
+    createdAt: true,
+  },
+    where: {
+      isDeleted: false,
+    },
+  });
 
   res.json({
     success: true,
@@ -12,4 +24,48 @@ router.get("/", async (req, res) => {
   });
 });
 
+router.get("/:id", async (req, res) => {
+  const user = await prisma.user.findUnique({
+    where: {
+      id: req.params.id,
+    },
+  });
+
+  res.json({
+    success: true,
+    data: user,
+  });
+});
+
+
+router.patch("/:id", async (req, res) => {
+  const updatedUser = await prisma.user.update({
+    where: {
+      id: req.params.id,
+    },
+    data: req.body,
+  });
+
+  res.json({
+    success: true,
+    data: updatedUser,
+  });
+});
+
+
+router.delete("/:id", async (req, res) => {
+  const deletedUser = await prisma.user.update({
+    where: {
+      id: req.params.id,
+    },
+    data: {
+      isDeleted: true,
+    },
+  });
+
+  res.json({
+    success: true,
+    data: deletedUser,
+  });
+});
 export default router;
